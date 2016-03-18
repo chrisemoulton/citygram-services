c322af4e752475e6709b417770e21ddec5c751d6
Meredith Underell <meredith.underell@socrata.com>
2016-03-18 10:16:58 -0700

    Added Douglas County Property Owner registry page

---
+++ lib/spy_glass/registry/douglas-county-property-owner.rb
@@ -0,0 +1,38 @@
+require 'spy_glass/registry'
+
+time_zone = ActiveSupport::TimeZone["Pacific Time (US & Canada)"]
+
+opts = {
+  path: '/douglas-county-property-owner',
+  cache: SpyGlass::Cache::Memory.new(expires_in: 300),
+  source: 'https://data.douglas.co.us/resource/e5ba-n74c?'+Rack::Utils.build_query({
+    '$limit' => 100
+  })
+}
+
+SpyGlass::Registry << SpyGlass::Client::Socrata.new(opts) do |collection|
+  features = collection.map do |item|
+    #latlng = item['location'].replace(/\(|\)/\g,'').split(",")
+    latlng = item['location'].gsub(/[()]/, '').split(/\s*,\s*/)
+    #puts latlngArr[0]
+    title = <<-TITLE
+      #{item['owner_name']}
+    TITLE
+
+    {
+      'id' => item['account_no'],
+      'type' => 'Feature',
+      'geometry' => {
+        'type' => 'Point',
+        'coordinates' => [
+          latlng[1].to_f,
+          latlng[0].to_f
+        ]
+      },
+      'properties' => item.merge('title' => title)
+    }
+  end
+
+  {'type' => 'FeatureCollection', 'features' => features}
+end
+
