e89c2399c327046f4fab15ffd086e4e89454202e
Meredith Underell <meredith.underell@socrata.com>
2016-03-18 10:49:04 -0700

    Added Douglas County Property Values dataset

--- lib/spy_glass/registry/douglas-county-property-values.rb
+++ lib/spy_glass/registry/douglas-county-property-values.rb
@@ -3,18 +3,23 @@ require 'spy_glass/registry'
 time_zone = ActiveSupport::TimeZone["Pacific Time (US & Canada)"]
 
 opts = {
-  path: '/douglas-county-property-owner',
+  path: '/douglas-county-property-values',
   cache: SpyGlass::Cache::Memory.new(expires_in: 300),
-  source: 'https://data.douglas.co.us/resource/e5ba-n74c?'+Rack::Utils.build_query({
+  source: 'https://data.douglas.co.us/resource/drva-83eq?'+Rack::Utils.build_query({
     '$limit' => 100
   })
 }
 
 SpyGlass::Registry << SpyGlass::Client::Socrata.new(opts) do |collection|
   features = collection.map do |item|
-    latlng = item['location'].gsub(/[()]/, '').split(/\s*,\s*/)
+    #longitude = item['location']['longitude']
+    #latitude = item['location']['latitude']
+    #.gsub(/[()]/, '').split(/\s*,\s*/)
+    #puts longitude
+    #puts latitude
     title = <<-TITLE
-      #{item['owner_name']}
+      Actual Value: #{item['actual_value']}
+      Assessed Value: #{item['assessed_value']}
     TITLE
 
     {
@@ -23,8 +28,8 @@ SpyGlass::Registry << SpyGlass::Client::Socrata.new(opts) do |collection|
       'geometry' => {
         'type' => 'Point',
         'coordinates' => [
-          latlng[1].to_f,
-          latlng[0].to_f
+          item['location']['longitude'].to_f,
+          item['location']['latitude'].to_f
         ]
       },
       'properties' => item.merge('title' => title)
