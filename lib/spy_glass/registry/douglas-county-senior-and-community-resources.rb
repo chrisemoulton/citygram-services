c73dd7b5c3b5195c9295518e2e29ac13a4a3ecea
Meredith Underell <meredith.underell@socrata.com>
2016-03-21 13:09:23 -0700

    Increased limit for all Douglas County datasets

--- lib/spy_glass/registry/douglas-county-senior-and-community-resources.rb
+++ lib/spy_glass/registry/douglas-county-senior-and-community-resources.rb
@@ -6,7 +6,7 @@ opts = {
   path: '/douglas-county-senior-and-community-resources',
   cache: SpyGlass::Cache::Memory.new(expires_in: 300),
   source: 'https://data.douglas.co.us/resource/t3ja-9fqv?'+Rack::Utils.build_query({
-    '$limit' => 100,
+    '$limit' => 1000,
     '$order' => 'organization_name DESC',
     '$where' => <<-WHERE.oneline
       web_page_address IS NOT NULL
